21a22e1ec1a24ad1ea848e18bf4d0b6d95034d5b
Navindex <mk@navindex.com.au>
2021-10-09 16:58:33 +0800

    Initial code version

---
+++ HtmlFormatter.php
@@ -0,0 +1,81 @@
+<?php
+
+use Navindex\HtmlFormatter\Formatter;
+
+/**
+ * HTML formatter plugin.
+ *
+ * Do not use this plugin in production environment.
+ *
+ * {@see https://github.com/navindex/html-formatter} for a full list of features.
+ *
+ * @author  Navindex Pty Ltd
+ * @link    https://navindex.com.au
+ * @license http://opensource.org/licenses/MIT The MIT License
+ * @version 0.1.0
+ */
+class HtmlFormatter extends AbstractPicoPlugin
+{
+    /**
+     * Configuration section key.
+     *
+     * @var array|null
+     */
+    protected $section = 'HtmlFormatter';
+
+    /**
+     * Plugin configuration
+     *
+     * @var array|null
+     */
+    protected $config;
+
+    /**
+     * API version used by this plugin
+     *
+     * @var int
+     */
+    const API_VERSION = 3;
+
+    /**
+     * @see AbstractPicoPlugin::$enabled
+     * @var bool|null
+     */
+    protected $enabled;
+
+    /**
+     * Formatter action (beautify or minify).
+     *
+     * @var string
+     */
+    protected $action = 'beautify';
+    /**
+     * Triggered after Pico has read its configuration
+     *
+     * @see Pico::getConfig()
+     * @see Pico::getBaseUrl()
+     * @see Pico::isUrlRewritingEnabled()
+     *
+     * @param array &$config array of config variables
+     */
+    public function onConfigLoaded(array &$config)
+    {
+        $this->config = $config[$this->section] ?? null;
+
+        $action = $this->config['action'] ?? null;
+        $this->action = in_array($action, ['beautify', 'minify']) ? $action : 'beautify';
+        unset($this->config['action']);
+    }
+
+    /**
+     * Triggered after Pico has rendered the page
+     *
+     * @see DummyPlugin::onPageRendering()
+     *
+     * @param string &$output contents which will be sent to the user
+     */
+    public function onPageRendered(&$output)
+    {
+        $output = (new Formatter($this->config))->beautify($output);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# pico-html-formatter
-Pico plugin to use the HTML formatter library.
+# HTML Formatter Pico Plugin [![Latest Version](https://img.shields.io/github/release/navindex/pico-html-formatter?sort=semver&label=version)](https://raw.githubusercontent.com/navindex/pico-html-formatter/master/CHANGELOG.md)
+
+## 1. What Is It
+
+This plugin is a wrapper to use [HTML Formatter](https://github.com/navindex/html-formatter) library with Pico.
+It will beautify or minify your HTML string for development and testing. Dedicated for those who suffer from reading a template engine produced markup.
+
+## 2. Installation
+
+This package can be installed through [Composer](https://getcomposer.org/).
+
+```bash
+composer require navindex/pico-html-formatter
+```
+
+## 3. Usage
+
+### Default settings
+
+You don't need to do anything. It works out of the box and eautifies the HTML output.
+
+### Custom settings
+
+Copy the content of `config.yml.template` file into your `config/config.yml` file and change the values.
+The settings are explained on the [navindex/html-formatter](https://github.com/navindex/html-formatter) Github page.
+
+## 4. About Navindex
+
+Navindex is a web development agency in Melbourne, Australia. You'll find an overview of our cmpany [on our website](https://www.navindex.com.au).
